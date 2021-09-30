31d856a68b0c374e54ef573d22af6d4d9e51a607
Alexandre <alexandre.szg@gmail.com>
2021-09-30 17:46:31 -0300

    Add basic weather-card

---
+++ weather-app/src/components/AppWeatherCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-weather-card">
+    <h2>{{ city }}</h2>
+    <div class="main-info">
+      <span>{{ temperature }}</span> - <span>{{ condition }}</span>
+    </div>
+    <div class="additional-info"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppWeatherCard",
+  props: {
+    city: {
+      type: String,
+      required: true,
+    },
+    temperature: {
+      type: Number,
+      required: true,
+    },
+    condition: {
+      type: String,
+      required: true,
+    },
+  },
+  data: function() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="sass">
+.app-weather-card
+  border: 1px solid grey
+  width: 250px
+  height: 350px
+</style>
--- weather-app/src/views/Home.vue
+++ weather-app/src/views/Home.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <ul class="city-list">
+      <li v-for="city in cities">
+        <app-weather-card
+          city="Cidade Aqui"
+          :temperature="30"
+          condition="Clear"
+        />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppWeatherCard from "@/components/AppWeatherCard.vue";
 
 export default defineComponent({
   name: "Home",
+  components: { AppWeatherCard },
+  data: function() {
+    return {
+      cities: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    };
+  },
 });
 </script>
 
-<style scoped lang="sass"></style>
+<style scoped lang="sass">
+ul.city-list
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+  gap: $space-medium
+  list-style-type: none
+  padding: 0
+  margin: 0
+</style>
