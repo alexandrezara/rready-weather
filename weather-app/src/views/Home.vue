281d0460c6c83392d944305049d3a7501616f9dc
Alexandre <alexandre.szg@gmail.com>
2021-10-03 17:12:38 -0300

    Add conversion to IWeather

--- weather-app/src/views/Home.vue
+++ weather-app/src/views/Home.vue
@@ -18,6 +18,8 @@
 import { defineComponent } from "vue";
 import AppWeatherCard from "@/components/AppWeatherCard.vue";
 import AppDraggableCard from "@/components/AppDraggableCard.vue";
+import { ICityWeather } from "@rready/weather-sdk";
+import { IWeather } from "@/model/IWeather";
 
 export default defineComponent({
   name: "Home",
@@ -49,13 +51,34 @@ export default defineComponent({
           .then((data) => {
             this.$store.commit("updateWeather", {
               city: item.cityName,
-              weather: data,
+              weather: this.convertWeather(data),
             });
           })
           .catch((error) => {
             alert(error);
           });
       });
+    },
+    convertWeather(response: ICityWeather): IWeather {
+      return {
+        temp: response.main.temp,
+        pressure: response.main.pressure,
+        humidity: response.main.humidity,
+        temp_min: response.main.temp_min,
+        temp_max: response.main.temp_max,
+        condition: {
+          name: response.weather[0].main,
+          description: response.weather[0].description,
+          icon: response.weather[0].icon,
+        },
+        visibility: response.visibility,
+        windSpeed: response.wind.speed,
+        windDeg: response.wind.deg,
+        country: response.sys.country,
+        sunrise: response.sys.sunrise,
+        sunset: response.sys.sunset,
+        timezone: response.timezone,
+      };
     },
   },
 });
